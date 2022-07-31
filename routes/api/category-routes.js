1e8b4ade14ff1b18e7d76e0bac22feaf5a67d8b4
DNA601 <dnavarrete6001@gmail.com>
2022-07-31 09:28:48 -0700

    updated files

---
+++ routes/api/category-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+    // find all categories
+    // be sure to include its associated Products
+    try {
+        const categories = await Category.findAll({ include: [Product] });
+        res.status(200).json(categories);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', (req, res) => {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    try {
+        const category = await Category.findByPk(req.params.id, {
+            // JOIN with locations, using the Trip through table
+            include: [Product]
+        });
+
+        if (!category) {
+            res.status(404).json({ message: 'No traveller found with this id!' });
+            return;
+        }
+
+        res.status(200).json(category);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+
+});
+
+router.post('/', (req, res) => {
+    // create a new category
+    try {
+        const category = await Category.create(req.body);
+        res.status(200).json(category);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', (req, res) => {
+    // update a category by its `id` value
+    try {
+        const category = await Category.update(req.body, {
+            where: { id: req.params.id }
+        });
+        if (!category) {
+            res.status(404).json({ message: 'No trip with this id!' });
+            return;
+        }
+        res.status(200).json(category);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    // delete a category by its `id` value
+    try {
+        const category = await Category.destroy({
+            where: { id: req.params.id }
+        });
+        if (!category) {
+            res.status(404).json({ message: 'No trip with this id!' });
+            return;
+        }
+        res.status(200).json(category);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+module.exports = router;
